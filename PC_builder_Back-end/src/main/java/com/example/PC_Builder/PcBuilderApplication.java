0f9b992912b536345084b770977caf05b8441e3a
Raivo <98113075+RaivoZTech@users.noreply.github.com>
2023-12-03 15:31:32 +0000

    Added front end code for project

---
+++ PC_builder_Back-end/src/main/java/com/example/PC_Builder/PcBuilderApplication.java
@@ -0,0 +1,15 @@
+package com.example.PC_Builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
+
+@SpringBootApplication
+public class PcBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcBuilderApplication.class, args);
+	}
+
+}
