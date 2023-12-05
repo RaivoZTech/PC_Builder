1e3f3d1e413aceeb44ee37cf5349204d6c256739
Raivo <98113075+RaivoZTech@users.noreply.github.com>
2023-12-05 21:50:28 +0000

    Added ComponentRepository interface for database operations

---
+++ PC_builder_Back-end/src/main/java/com/example/PC_Builder/respository/ComponentRepository.java
@@ -0,0 +1,11 @@
+package com.example.PC_Builder.respository;
+
+import com.example.PC_Builder.entity.Components;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface ComponentRepository extends JpaRepository<Components, Long> {
+    List<Components> findByType(String type);
+}
